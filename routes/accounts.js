dbec6584fcbac76657ba7d66a01dcc0a64de2a91
Maarja <maarja.tapsi@gmail.com>
2020-09-28 09:40:17 +0300

    As a user I can see my account details [Delivers #3]

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,21 +2,23 @@ const User = require('../models/User');
 const Account = require('../models/Account');
 const express = require('express');
 const router = express.Router();
+const verify = require('./tokenVerify');
 
-router.get('/', async (req, res, next) => {
+
+router.get('/', verify, async (req, res, next) => {
+
     try {
-        const account = await Account.find({
-            user: res.locals.loggedInUser._id
+        const account = await Account.findOne({
+            user: req.user._id
+        });
+        if(!account) return res.status(400).json({error: "No account was found"});
+        res.status(200).json({
+            account_info: account
         })
-        if (!account) return next(new Error('This user doesnt have an account.'));
-        res.status(201).json({
-            message: "Success",
-            code: 201,
-            account_data: account
-          });
-    } catch (error) {
-        next(error);
+    } catch (e) {
+        next(e);
     }
 });
+
 
 module.exports = router;
